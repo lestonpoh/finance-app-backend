3eef412f18afb997231f4488426adea0a49897c6
Wang Anqi <129874349+lestonpoh@users.noreply.github.com>
2025-11-22 20:22:28 +0800

    fixes

--- src/main/java/com/lestonpoh/financeapp_backend/config/RedisConfig.java
+++ src/main/java/com/lestonpoh/financeapp_backend/config/RedisConfig.java
@@ -12,9 +12,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${redis.host}")
     private String host;
-    @Value("${spring.redis.port}")
+    @Value("${redis.port}")
     private Integer port;
 
     @Bean
