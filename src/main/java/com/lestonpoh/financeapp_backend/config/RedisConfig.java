8dbc3450f24a537ba8dea088e77707ef170db36b
= <129874349+lestonpoh@users.noreply.github.com>
2025-11-22 12:00:28 +0800

    fized redis config and compose file

--- src/main/java/com/lestonpoh/financeapp_backend/config/RedisConfig.java
+++ src/main/java/com/lestonpoh/financeapp_backend/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package com.lestonpoh.financeapp_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,9 +12,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private Integer port;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
